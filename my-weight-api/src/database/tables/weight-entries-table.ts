32c49924b5894fe9fb8417c866a5127d1cab3b1f
Marco Preciado <mac46005@gmail.com>
2024-08-04 13:27:45 +0200

    Functionality added to WeightEntriesTable.create()

--- my-weight-api/src/database/tables/weight-entries-table.ts
+++ my-weight-api/src/database/tables/weight-entries-table.ts
@@ -4,6 +4,7 @@ import SqlTable from "../util/classes/sql-table";
 import IMySqlInfo from "../util/interfaces/i-mysql-info";
 import ISqlResult from "../util/interfaces/i-sql-result";
 import IUserEntry from "../types/utils/i-user-entry";
+import { SqlFunctions } from "../util/enums/i-sql-functions";
 
 @injectable()
 export default class WeightEntriesTable extends SqlTable<IWeightEntry> {
@@ -15,24 +16,59 @@ export default class WeightEntriesTable extends SqlTable<IWeightEntry> {
                 ID: 'id',
                 USER_ID: 'user_id',
                 WEIGHT: 'weight',
-                TIMESTAMP: 'timestamp',
-                NOTES: 'note',
+                NOTE: 'note',
                 CREATED_ON: 'created_on',
                 UPDATED_ON: 'updated_on'
             }
         );
     }
-    create(item: IWeightEntry): Promise<ISqlResult<IWeightEntry>> {
-        throw new Error("Method not implemented.");
+    async create(item: IWeightEntry): Promise<ISqlResult<IWeightEntry>> {
+        let sqlResult : ISqlResult<IWeightEntry>;
+        try {
+            const sqlStatement = this.sqlStmtProcessor.create(
+                columns => `${columns.USER_ID}, ${columns.WEIGHT}, ${columns.NOTE}`,
+                () => `${item.user_id}, ${item.weight}, "${item.note}"`
+            );
+
+            console.log(sqlStatement);
+
+            sqlResult = await this.getResult(SqlFunctions.CREATE, sqlStatement);
+
+        } catch (err) {
+            throw err;
+        }
+
+        return sqlResult;
     }
-    read(item?: IUserEntry | undefined): Promise<ISqlResult<IWeightEntry>> {
-        throw new Error("Method not implemented.");
+    async read(item?: IUserEntry | undefined): Promise<ISqlResult<IWeightEntry>> {
+        let sqlResult : ISqlResult<IWeightEntry>;
+        try {
+
+        } catch (err) {
+            throw err;
+        }
+
+        return sqlResult;
     }
-    update(item: IWeightEntry): Promise<ISqlResult<IWeightEntry>> {
-        throw new Error("Method not implemented.");
+    async update(item: IWeightEntry): Promise<ISqlResult<IWeightEntry>> {
+        let sqlResult : ISqlResult<IWeightEntry>;
+        try {
+
+        } catch (err) {
+            throw err;
+        }
+
+        return sqlResult;
     }
-    delete(item: IUserEntry): Promise<ISqlResult<IWeightEntry>> {
-        throw new Error("Method not implemented.");
+    async delete(item: IUserEntry): Promise<ISqlResult<IWeightEntry>> {
+        let sqlResult : ISqlResult<IWeightEntry>;
+        try {
+
+        } catch (err) {
+            throw err;
+        }
+
+        return sqlResult;
     }
    
     
