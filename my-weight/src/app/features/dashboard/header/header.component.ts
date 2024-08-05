478070ffbd44dcc2f4673d438c8438409c84a49b
Marco Preciado <mac46005@gmail.com>
2024-08-05 22:43:05 +0200

    Adding header to features/dashboard

---
+++ my-weight/src/app/features/dashboard/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header[main-header]',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
